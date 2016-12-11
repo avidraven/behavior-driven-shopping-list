e66e0eadf477942559cfbc586a5b38ad1012048a
David Craven <davidcraven@Davids-MacBook-Pro-2.local>
2016-12-10 18:57:01 -1000

    initial setup

--- js/shopping_list_item_test.js
+++ js/shopping_list_item_test.js
@@ -1,1 +1,28 @@
+/*jshint esversion: 6 */
+
 console.log("shopping item test");
+
+// const chai = require("chai"); //node only
+
+const expect = chai.should();
+const should = chai.should();
+
+describe("ShoppingListItem", () =>{
+  // var item;
+  // beforeEach(function(){
+  //   item = new ShoppingListItem(name, description);
+  // });
+  // let item = new Zombies.item("ted"); //use new instance for testing
+  let listItem = new ShoppingListItem();
+  it("should be a class", () =>{
+    listItem.should.be.a.function;
+  });
+
+  // it("expect ShoppingListItem to have a name", () =>{
+  //   expect(ShoppingListItem).to.have.property("name");
+  // });
+
+  // it("expect ShoppingListItem to have a description", () =>{
+  //   expect(ShoppingListItem).to.have.property("description");
+  // });
+});
