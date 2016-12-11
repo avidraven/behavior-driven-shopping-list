03666fd39f80614e91008b4e80e19aa050c26a62
David Craven <davidcraven@Davids-MacBook-Pro-2.local>
2016-12-10 19:51:08 -1000

    5 tests done

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -1,5 +1,19 @@
 class ShoppingListItem {
-  constructor(){
+  constructor(name, description){
+    this.name = name;
+    this.description = description;
+    this.is_done = false;
+  }
+
+  check(){
+    this.is_done = true;
+  }
+
+  uncheck(){
+    this.is_done = false;
+  }
+
+  render(){
 
   }
 }
--- js/shopping_list_item_test.js
+++ js/shopping_list_item_test.js
@@ -4,7 +4,7 @@ console.log("shopping item test");
 
 // const chai = require("chai"); //node only
 
-const expect = chai.should();
+const expect = chai.expect;
 const should = chai.should();
 
 describe("ShoppingListItem", () =>{
@@ -13,16 +13,24 @@ describe("ShoppingListItem", () =>{
   //   item = new ShoppingListItem(name, description);
   // });
   // let item = new Zombies.item("ted"); //use new instance for testing
-  let listItem = new ShoppingListItem();
+  let listItem = new ShoppingListItem("bread", "baked and soft");
   it("should be a class", () =>{
     listItem.should.be.a.function;
   });
 
-  // it("expect ShoppingListItem to have a name", () =>{
-  //   expect(ShoppingListItem).to.have.property("name");
-  // });
+  it("expect ShoppingListItem to have a name", () =>{
+    expect(listItem).to.have.property("name");
+  });
 
-  // it("expect ShoppingListItem to have a description", () =>{
-  //   expect(ShoppingListItem).to.have.property("description");
-  // });
+  it("expect ShoppingListItem to have a description", () =>{
+    expect(listItem).to.have.property("description");
+  });
+
+  it("expect ShoppingListItem to have an is_done property", () =>{
+    expect(listItem).to.have.property("is_done");
+  });
+
+  it("expect ShoppingListItem to have 2 arguments", () =>{
+    expect(listItem.constructor).to.have.length(2);
+  });
 });
