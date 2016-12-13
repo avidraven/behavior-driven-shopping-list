60afae65b86840913a8adbe8a41c1746b5225f2b
jonathonlaylo <jonathon.laylo@gmail.com>
2016-12-12 22:37:59 -1000

    crate add_to_shopping_list function and title, description, and new_shopping_list_item variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 var myShoppingList = new ShoppingList();
+
+function add_to_shopping_list() {
+  var title = document.getElementById('title').value;
+  var description = '' + document.getElementById('description').value;
+  var new_shopping_list_item = new ShoppingListItem(title, description);
+}
