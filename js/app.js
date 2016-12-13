2e850b5d7e8bce25c04c8fef6efc1bd3f9a24406
jonathonlaylo <jonathon.laylo@gmail.com>
2016-12-12 22:42:35 -1000

    invoved addItem and passed new_shopping_list_item

--- js/app.js
+++ js/app.js
@@ -4,4 +4,6 @@ function add_to_shopping_list() {
   var title = document.getElementById('title').value;
   var description = '' + document.getElementById('description').value;
   var new_shopping_list_item = new ShoppingListItem(title, description);
+
+  myShoppingList.addItem(new_shopping_list_item);
 }
