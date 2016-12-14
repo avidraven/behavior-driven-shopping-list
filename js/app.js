164cea561c5764e60ac1337f55f2d9aaf034982c
jonathonlaylo <jonathon.laylo@gmail.com>
2016-12-13 23:06:18 -1000

    current changes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 var myShoppingList = new ShoppingList();
 
+
 function add_to_shopping_list() {
   var title = document.getElementById('title').value;
   var description = '' + document.getElementById('description').value;
   var new_shopping_list_item = new ShoppingListItem(title, description);
-
   myShoppingList.addItem(new_shopping_list_item);
   console.log(myShoppingList);
+
+  myShoppingList.render(new_shopping_list_item);
 }
 
 function changeCheckedStatus(idx, checkbox) {
