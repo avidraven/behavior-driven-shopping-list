f61faa5151949edc411761a814da5c257cb6a4dc
jonathonlaylo <jonathon.laylo@gmail.com>
2016-12-14 17:53:40 -1000

    updated removeItemButtonClicked

--- js/app.js
+++ js/app.js
@@ -1,28 +1,37 @@
 var myShoppingList = new ShoppingList();
 
+var renderedList = myShoppingList.render();
+content.innerHTML = renderedList;
 
 function add_to_shopping_list() {
-  var title = document.getElementById('title').value;
-  var description = '' + document.getElementById('description').value;
-  var new_shopping_list_item = new ShoppingListItem(title, description);
-  myShoppingList.addItem(new_shopping_list_item);
-  console.log(myShoppingList);
+ item_buttons.innerHTML = null;  //added so extra buttons and checkboxes don't show up
+ var title = document.getElementById('title').value;
+ var description = '' + document.getElementById('description').value;
+ var new_shopping_list_item = new ShoppingListItem(title, description);
 
-  myShoppingList.render(new_shopping_list_item);
+ myShoppingList.addItem(new_shopping_list_item);
+ console.log(myShoppingList);
+
+ renderedList = myShoppingList.render();
+ content.innerHTML = renderedList;
 }
 
 function changeCheckedStatus(idx, checkbox) {
-  var item = myShoppingList.item[idx];
-  //if the checkbox is checked, invoke shopping_list_items check()
-  if(checkbox.checked() === true){
-    item.check();
-  } else {
-  //if the checkbox is not checked, invoke the shopping_list_items check()
-    item.uncheck();
-  }
+ let item = myShoppingList.items[idx];
+ //if the checkbox is checked, invoke shopping_list_items check()
+ if(checkbox.checked() === true){
+   item.check();
+ } else {
+ //if the checkbox is not checked, invoke the shopping_list_items check()
+   item.uncheck();
+ }
 }
 
 function removeItemButtonClicked(idx) {
-  var item = myShoppingList.item[idx];
-  myShoppingList.removeItem(item);
+ let item = myShoppingList.items[idx];
+ console.log("remove item", item);
+ myShoppingList.removeItem(item);
+
+ renderedList = myShoppingList.render();
+ content.innerHTML = renderedList;
 }
