64518613281d71873ce298666e2ed1f3a957cb1d
jonathonlaylo <jonathon.laylo@gmail.com>
2016-12-14 17:54:55 -1000

    updated render for removeElement

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -1,31 +1,37 @@
 class ShoppingListItem {
-  constructor(name, description){
-    this.name = name;
-    this.description = description;
-    this.is_done = false;
-  }
+ constructor(name, description){
+   this.name = name;
+   this.description = description;
+   this.is_done = false;
+ }
 
-  check(){
-    this.is_done = true;
-  }
+ check(){
+   this.is_done = true;
+ }
 
-  uncheck(){
-    this.is_done = false;
-  }
+ uncheck(){
+   this.is_done = false;
+ }
 
-  render(){
-    var checkboxElement = document.createElement("input");
-    checkboxElement.setAttribute("type", "checkbox");
-    checkboxElement.id = "checkbox";
-    content.appendChild(checkboxElement);
-    checkboxElement.addEventListener("change", changedCheckedStatus);
+ render(){
+   console.log("item render", myShoppingList.items.indexOf(this));
 
-    var removeElement = document.createElement("button");
-    removeElement.id = "Xbutton";
-    removeElement.innerHTML = "X";
-    content.appendChild(removeElement);
-    removeElement.addEventListener("click", removeItemButtonClicked(idx));
+   var checkboxElement = document.createElement("input");
+   checkboxElement.setAttribute("type", "checkbox");
+   checkboxElement.className = "checkbox";
+   item_buttons.appendChild(checkboxElement);
+   checkboxElement.addEventListener("change", function(){
+     changeCheckedStatus(myShoppingList.items.indexOf(this), checkboxElement);
+   });
 
-    return '<li class="completed_'+ this.is_done + '"><span>' + this.name + '</span><span>' + this.description + '</span></li>';
-    }
+   var removeElement = document.createElement("button");
+   removeElement.className = "Xbutton";
+   removeElement.innerHTML = "X";
+   item_buttons.appendChild(removeElement);
+   removeElement.addEventListener("click", function(){
+     removeItemButtonClicked(myShoppingList.items.indexOf(this));
+   });
+
+   return `<li class="completed_${this.is_done}"><span>${this.name}:</span> <span>${this.description}</span></li>`;
+   }
 }
